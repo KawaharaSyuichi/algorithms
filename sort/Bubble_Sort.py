85db7361a750f2305ec1da33e70779a76bb707fb
ShuitiKawahara <kawaharasyuichi@gmail.com>
2020-08-15 00:06:03 +0900

    add bubble sort program

---
+++ sort/Bubble_Sort.py
@@ -0,0 +1,31 @@
+def bubblesort(A, N):
+    sw = 0
+    flag = True
+    i = 0
+
+    while flag:
+        flag = False
+
+        for j in reversed(range(N)):
+            if j < i + 1:
+                break
+
+            if A[j] < A[j - 1]:
+                temp = A[j]
+                A[j] = A[j - 1]
+                A[j - 1] = temp
+                flag = True
+                sw += 1
+
+        i += 1
+
+    return sw
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+sw = bubblesort(A, N)
+
+print(A)
+print(sw, end='')
